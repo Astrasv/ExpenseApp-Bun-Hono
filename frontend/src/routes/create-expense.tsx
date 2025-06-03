ed4b45474d773e95a72b65924fa141f5b88d0bd3
Astrasv <116169969+Astrasv@users.noreply.github.com>
2025-06-03 12:06:07 +0530

    Create form Frontend

--- frontend/src/routes/create-expense.tsx
+++ frontend/src/routes/create-expense.tsx
@@ -1,9 +1,26 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Input } from '@/components/ui/input'
+import { Label } from '@radix-ui/react-label'
+import { Button } from '@/components/ui/button'
+
+
+
 
 export const Route = createFileRoute('/create-expense')({
   component: CreateExpense,
 })
 
 function CreateExpense() {
-  return <div className="p-2">Hello from CreateExpense!</div>
+  return (
+    <div className="p-2">
+       <h2>CreateExpense</h2>
+       <form action="" className='max-w-xl m-auto'>
+        <Label htmlFor="title">Title</Label>
+        <Input type="text" id="title" placeholder="Title" />
+        <Label htmlFor="amount">Amount</Label>
+        <Input type="number" id="amount" placeholder="Amount" />
+        <Button type="submit" className='mt-4 '>Submit</Button>
+       </form>
+    </div>
+  )
 }
--- frontend/src/routes/expenses.tsx
+++ frontend/src/routes/expenses.tsx
@@ -12,6 +12,7 @@ import {
     TableHeader,
     TableRow,
   } from "@/components/ui/table"
+import { Skeleton } from '@/components/ui/skeleton'
 
 export const Route = createFileRoute('/expenses')({
   component: Expenses,
@@ -47,7 +48,18 @@ function Expenses() {
             </TableRow>
         </TableHeader>
         <TableBody>
-            {isPending ? "Loading..." : data?.expenses.map((expense: any) => (
+            {isPending 
+              ? Array(3).fill(0).map((_,i) => (
+                <TableRow key={i}>
+                  <TableCell className="font-medium"><Skeleton className="h-4" /></TableCell>
+                  <TableCell><Skeleton className="h-4" /></TableCell>
+                  <TableCell><Skeleton className="h-4" /></TableCell>
+                </TableRow>
+
+              )) 
+              
+              
+              : data?.expenses.map((expense: any) => (
                 <TableRow key={expense.id}>
                     <TableCell className="font-medium">{expense.id}</TableCell>
                     <TableCell>{expense.title}</TableCell>
